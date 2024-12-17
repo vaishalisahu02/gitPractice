e64c333e6fc929fa867d41b8ef7155b8341ba8e6
ayushudaydubey <102001ayushdubey@gmail.com>
2024-12-17 09:58:14 +0530

    upadte

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-console.log("hello")
-console.log("hello")
-console.log("ayush")
+var a =12; // global scopic variable
+let b =23; //local scopic variable
+const c =34; // local  scopic constant variable
+a =13;
+console.log (a) // give error 
 
